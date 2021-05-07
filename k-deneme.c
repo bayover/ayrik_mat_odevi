0069b6933a8584e0e0017ab90aed85baea770be4
bayover <mustafasucu06@gmail.com>
2021-05-08 01:01:45 +0300

    odevim

---
+++ k-deneme.c
@@ -0,0 +1,180 @@
+#include<stdio.h>
+
+struct veriSeti
+{
+	char koyler[15];
+	int nufuz;
+	char kumeK[15];
+	int kumeN;
+}veri[30],kume[30];
+
+void veriCek();
+void kumeOlustur();
+void altKumeVerisiAl();
+void ustKumeVerisiAl();
+void ustKumeYazdir();
+void altKumeYazdir();
+
+int i,temp,sonuc1,sonuc2,baraj=5000;
+
+main()
+{
+	int t=0;
+	veriCek();
+	kumeOlustur();
+	printf("Belediye olabilecek ve olamayacak koyler\n\n");
+	printf("Baraj siniri: %d\n",baraj);
+	printf("Belediye olabilecek koyler ilk kume sirasi:\n");
+	ustKumeYazdir();
+	printf("Belediye olamayacak koyler ilk kume sirasi:\n");
+	altKumeYazdir();
+	
+	while(t!=1)
+	{
+	  veriCek();
+      kumeOlustur();
+	  altKumeVerisiAl();
+	  ustKumeVerisiAl();
+	  baraj=(sonuc1+sonuc2)/2;
+	  printf("Ara barajlar(ortalamalar): %d\n",baraj);
+	  if(temp==baraj)
+	  {
+	  	t=1;
+	  	printf("Baraj siniri(ortalama) esit oldu\n");
+	  	printf("\nBaraj siniri: %d\n",baraj);
+	  	printf("Belediye olabilecek koylerin k-means ile siralandiktan sonraki sirasi:\n");
+	  	ustKumeYazdir();
+	  	printf("Belediye olamayacak koylerin k-means ile siralandiktan sonraki sirasi:\n");
+	    altKumeYazdir();
+	  }
+	  else
+	  {
+	  	temp=baraj;
+	  }
+	}	
+}
+//-----------------------------------------------------------------------------------------------
+void veriCek()
+{
+	FILE *file;
+	if ((file = fopen("veri_seti.txt", "r")) == NULL) 
+    {
+        return -1; 
+    }
+    int a=0;
+    while(!feof(file))
+    {
+    	fscanf(file,"%s%d",&veri[a].koyler,&veri[a].nufuz);
+        a++;
+	}
+	fclose(file);	
+	return 0;
+}
+//-----------------------------------------------------------------------------------------------
+void kumeOlustur()
+{
+	int a=0;
+	FILE *yazdir,*yazdir2;
+    yazdir=fopen("nufuz_baraj_alti.txt","w");
+    yazdir2=fopen("nufuz_baraj_ustu.txt","w");
+	for(i=0;i<30;i++)
+	{
+		if(veri[a].nufuz<baraj)
+    	{
+    		fprintf(yazdir,"%s %d\n",veri[i].koyler,veri[i].nufuz);
+    		a++;
+		}
+		else if(veri[a].nufuz>=baraj)
+		{
+    		fprintf(yazdir2,"%s %d\n",veri[i].koyler,veri[i].nufuz);
+    		a++;
+		}
+	}
+	fclose(yazdir);
+	fclose(yazdir2);
+	return 0;
+}
+//-----------------------------------------------------------------------------------------------
+void altKumeVerisiAl()
+{
+	FILE *file;
+	if ((file = fopen("nufuz_baraj_alti.txt", "r")) == NULL) 
+    {
+        return -1; 
+    }
+    int a=0,toplam=0;
+    while(!feof(file))
+    {
+    	fscanf(file,"%s%d",&kume[a].kumeK,&kume[a].kumeN);
+        a++;
+	}
+	fclose(file);
+	for(i=0;i<(a-1);i++)
+	{
+		toplam=toplam+kume[i].kumeN;
+	}
+	sonuc2=toplam/(a-1);
+}
+//-----------------------------------------------------------------------------------------------
+void ustKumeVerisiAl()
+{
+	FILE *file;
+	if ((file = fopen("nufuz_baraj_ustu.txt", "r")) == NULL) 
+    {
+        return -1; 
+    }
+    int a=0,toplam=0;
+    while(!feof(file))
+    {
+    	fscanf(file,"%s%d",&kume[a].kumeK,&kume[a].kumeN);
+        a++;
+	}
+	fclose(file);
+	for(i=0;i<(a-1);i++)
+	{
+		toplam=toplam+kume[i].kumeN;
+	}
+	sonuc1=toplam/(a-1);
+}
+//-----------------------------------------------------------------------------------------------
+void ustKumeYazdir()
+{
+	FILE *file;
+	if ((file = fopen("nufuz_baraj_ustu.txt", "r")) == NULL) 
+    {
+        return -1; 
+    }
+    int a=0;
+    while(!feof(file))
+    {
+    	fscanf(file,"%s%d",&kume[a].kumeK,&kume[a].kumeN);
+        a++;
+	}
+	fclose(file);
+	for(i=0;i<(a-1);i++)
+	{
+		printf("%d-%s   Nufuz-->%d\n",i+1,kume[i].kumeK,kume[i].kumeN);
+	}
+	printf("\n**************************************************\n\n");
+}
+//-----------------------------------------------------------------------------------------------
+void altKumeYazdir()
+{
+	FILE *file;
+	if ((file = fopen("nufuz_baraj_alti.txt", "r")) == NULL) 
+    {
+        return -1; 
+    }
+    int a=0;
+    while(!feof(file))
+    {
+    	fscanf(file,"%s%d",&kume[a].kumeK,&kume[a].kumeN);
+        a++;
+	}
+	fclose(file);
+	for(i=0;i<(a-1);i++)
+	{
+		printf("%d-%s   Nufuz-->%d\n",i+1,kume[i].kumeK,kume[i].kumeN);
+	}
+    printf("\n**************************************************\n\n");
+}
